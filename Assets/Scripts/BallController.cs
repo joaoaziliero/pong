ac2de694675c9ec55883534b02bc544d1a34404f
joaoaziliero <146762748+joaoaziliero@users.noreply.github.com>
2023-12-24 20:18:37 -0300

    Update BallController.cs

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class BallController : MonoBehaviour
 {
+    public GameManager gameManager;
+
     private Rigidbody2D _rb;
     [SerializeField] private Vector2 _initialVelocity = new Vector2(5, 5);
 
@@ -34,6 +36,21 @@ public class BallController : MonoBehaviour
             case "Enemy":
                 _rb.velocity = new Vector2((+1) * _initialVelocity.x,
                     _initialVelocity.y);
+                break;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        switch (collision.gameObject.tag)
+        {
+            case "EnemyWall":
+                gameManager.ScorePlayer();
+                ResetBall();
+                break;
+            case "PlayerWall":
+                gameManager.ScoreEnemy();
+                ResetBall();
                 break;
         }
     }
